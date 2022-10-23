8d3da91e47560d09693d7dd0a3a402c862d11032
Khiem G Luong <78592170+smarttarded@users.noreply.github.com>
2022-10-23 12:47:52 -0700

    Refactoring again

--- src/main/java/com/i_comit/linux/AES.java
+++ src/main/java/com/i_comit/linux/AES.java
@@ -84,8 +84,7 @@ public class AES {
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
             cipher.init(cipherMode, secretKey);
 
-            try ( FileInputStream inputStream = new FileInputStream(inputFile)) {
-                FileOutputStream outputStream = new FileOutputStream(outputFile);
+            try ( FileInputStream inputStream = new FileInputStream(inputFile); FileOutputStream outputStream = new FileOutputStream(outputFile)) {
                 byte[] inputBytes = new byte[(int) inputFile.length()];
                 int nread;
                 while ((nread = inputStream.read(inputBytes)) > 0) {
@@ -94,6 +93,7 @@ public class AES {
                 }
                 byte[] enc = cipher.doFinal();
                 outputStream.write(enc);
+                inputStream.close();
                 outputStream.close();
             }
         } catch (NoSuchPaddingException | NoSuchAlgorithmException
